f3d42bd902485c1c323cc8c75b0cbe42a36baed7
Glitch (hello-webpage) <none>
2020-10-13 19:05:42 +0000

    🐲💁 Checkpoint ./sketch.js:574715/1819 ./script.js:574715/70 ./index.html:574715/1559

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(600, 500);
+}
+
+function draw() {
+  background(230);
+  // Delete the slashes on the next line to "uncomment" the ellipse
+  // ellipse(50, 50, 50, 50)
+}
